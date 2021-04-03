9a7376be73754b4d9417493add5c83d842e01c35
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-04-03 21:20:45 +0200

    updated

--- src/main/java/com/example/superheroes/config/CorsConfig.java
+++ src/main/java/com/example/superheroes/config/CorsConfig.java
@@ -2,6 +2,7 @@ package com.example.superheroes.config;
 
 import java.util.Arrays;
 import java.util.Collections;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
--- src/main/java/com/example/superheroes/utils/JwtUtility.java
+++
@@ -1,5 +0,0 @@
-package com.example.superheroes.utils;
-
-import java.io.Serializable;
-
-public class JwtUtility implements Serializable {}
