00b873df271b8ad750ac7fe4333a5b92ec2b3ecc
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-12-05 19:07:38 +0100

    update

--- frontend/cypress/global.d.ts
+++ frontend/cypress/global.d.ts
@@ -2,10 +2,10 @@
 
 declare namespace Cypress {
   interface Chainable {
-    getCommand(url: string, responseBody: Array<T>): Chainable<T>;
-    deleteCommand(url: string): Chainable<any>;
-    postCommand(url: string, requestBody: T): Chainable<T>;
-    SetupInputFieldsCommand(): Chainable<T>;
-    NavigateByTestIdCommand(testId: string): Chainable<T>;
+    getCommand(url: string, responseBody: Array<T>): void;
+    deleteCommand(url: string): void;
+    postCommand(url: string, requestBody: T): void;
+    SetupInputFieldsCommand(): void;
+    NavigateByTestIdCommand(testId: string): void;
   }
 }
