bbff6db085d9718965fcc032cf3dbe4a754b2bb4
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-12-19 14:34:11 +0100

    update

--- src/main/java/com/example/superheroes/config/SwaggerConfig.java
+++ src/main/java/com/example/superheroes/config/SwaggerConfig.java
@@ -7,10 +7,8 @@ import io.swagger.v3.oas.models.info.License;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile({"!prod && swagger"})
 public class SwaggerConfig {
 
     @Bean
